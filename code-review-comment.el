7da5481d0702695c1ff5c5ca1c0949dc8bbcbdf1
Maxim Bunkov <bimawa@gmail.com>
2026-06-01 20:25:40 +0500

    fix: add multiline lambda

--- code-review-comment.el
+++ code-review-comment.el
@@ -216,7 +216,7 @@ Optionally define a MSG."
                               (save-excursion
                                 (goto-char region-start)
                                 (string-join
-                                 (-map (lambda (l) (substring l 1))
+                                 (mapcar (lambda (l) (substring l 1))
                                        (split-string
                                         (buffer-substring-no-properties
                                          (save-excursion
